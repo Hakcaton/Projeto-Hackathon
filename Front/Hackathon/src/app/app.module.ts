bb3f0e66953d171e829f33d4cbed34addb611023
Carlos <carlos.dev.carvalho@gmail.com>
2022-03-26 15:07:48 -0300

    Organização e criação das pastas iniciais

--- Front/Hackathon/src/app/app.module.ts
+++ Front/Hackathon/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginModuleComponent } from './modules/login-module/login-module.component';
+import { PerfilModuleComponent } from './modules/perfil-module/perfil-module.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginModuleComponent,
+    PerfilModuleComponent,
+  
   ],
   imports: [
     BrowserModule,
