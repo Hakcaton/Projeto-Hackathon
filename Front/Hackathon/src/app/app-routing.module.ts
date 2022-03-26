ac735dd488256a055035a088a3fabac54385663d
Carlos <carlos.dev.carvalho@gmail.com>
2022-03-26 15:50:24 -0300

    Alteracao diversas

--- Front/Hackathon/src/app/app-routing.module.ts
+++ Front/Hackathon/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginModuleComponent } from './modules/login-module/login-module.component';
+import { PerfilModuleComponent } from './modules/perfil-module/perfil-module.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "login", component: LoginModuleComponent},
+  {path: "perfil", component: PerfilModuleComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
