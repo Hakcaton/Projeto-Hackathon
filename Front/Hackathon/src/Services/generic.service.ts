81a4ebb51eac5573e8a801b2a612932612bf3305
GustavoDBP <gdbperes@gmail.com>
2022-04-03 13:28:13 -0300

    Sem Tarefa
    Correção da função responsável por calcular a força da senha

--- Front/Hackathon/src/Services/generic.service.ts
+++ Front/Hackathon/src/Services/generic.service.ts
@@ -6,15 +6,15 @@ export class GenericService {
     
     constructor(){}
 
-    public validarForcaSenha(senha): number{
+    public validarForcaSenha(senha: string): number{
         let forca = 0;
 
         if(senha.length > 0 && senha.length < 4) forca += 1;
         else if (senha.length >= 4) forca += 10;
-        if(senha.math(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) forca += 10; // Letras Maiusculas e minusculas
-        if(senha.math(/([0-9])/)) forca += 10; // Numeros
-        if(senha.math(/\W/)) //Caracteres especiais
-        if((senha.math(/([0-9])/)) && (senha.math(/\W/)) && (senha.math(/([A-Z])/)) && (senha.length >=7)) forca += 60;
+        if(senha.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) forca += 10; // Letras Maiusculas e minusculas
+        if(senha.match(/([0-9])/)) forca += 10; // Numeros
+        if(senha.match(/\W/)) //Caracteres especiais
+        if((senha.match(/([0-9])/)) && (senha.match(/\W/)) && (senha.match(/([A-Z])/)) && (senha.length >=7)) forca += 60;
 
         return forca;
     }
